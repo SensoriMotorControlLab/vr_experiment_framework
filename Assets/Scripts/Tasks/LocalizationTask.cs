fccee6464326d595f2e6e9a1323f720f47db53ff
Jorge de Oliveira <jorge.esteves.junior@gmail.com>
2022-11-14 10:10:20 -0500

    Localizer rotation and location fixed

--- Assets/Scripts/Tasks/LocalizationTask.cs
+++ Assets/Scripts/Tasks/LocalizationTask.cs
@@ -43,10 +43,10 @@ public class LocalizationTask : BaseTask
         ctrler.CursorController.SetCursorVisibility(true);
 
         localizationPrefab = Instantiate(ctrler.GetPrefab("LocalizationPrefab"));
-        localizationPrefab.transform.parent = ctrler.transform;
-        localizationPrefab.transform.position = Vector3.zero;
-        ctrler.TargetContainer.transform.localPosition = Vector3.zero;
         
+        localizationPrefab.transform.position = Vector3.zero;
+        localizationPrefab.transform.SetParent(ctrler.transform);
+        ctrler.TargetContainer.transform.position = Vector3.zero;
 
         localizationCam = GameObject.Find("LocalizationCamera");
         localizationSurface = GameObject.Find("Surface");
@@ -55,12 +55,12 @@ public class LocalizationTask : BaseTask
 
         // Set up the dock position
         targets[0] = GameObject.Find("Dock");
-        targets[0].transform.localPosition = ctrler.TargetContainer.transform.localPosition - ctrler.transform.forward * dock_dist;
+        targets[0].transform.localPosition = ctrler.TargetContainer.transform.position - ctrler.transform.forward * dock_dist;
         //targets[0].transform.position = new Vector3(ctrler.TargetContainer.transform.position.x, -0.250f, ctrler.TargetContainer.transform.position.z);
 
         // Set up the home position
         targets[1] = GameObject.Find("Home");
-        targets[1].transform.localPosition = ctrler.TargetContainer.transform.localPosition;
+        targets[1].transform.localPosition = ctrler.TargetContainer.transform.position;
         //targets[1].transform.position = new Vector3(ctrler.TargetContainer.transform.position.x, -0.250f, ctrler.TargetContainer.transform.position.z) + ctrler.transform.forward * 0.05f;
         targets[1].SetActive(false);
         Home = targets[1];
@@ -85,14 +85,15 @@ public class LocalizationTask : BaseTask
 
         // Set up the GameObject that tracks the user's gaze
         locAim = GameObject.Find("Localizer");
+        locAngle = GameObject.Find("locAngle");
         
         locAim.GetComponent<SphereCollider>().enabled = false;
         locAim.GetComponent<BaseTarget>().enabled = false;
         locAim.SetActive(false);
 
         locAim.transform.position =locAim.transform.position + Vector3.forward * 12.3f/100f;
-        locAim.transform.parent = ctrler.TargetContainer.transform;
-        //ctrler.TargetContainer.transform.position = targets[1].transform.position;
+        
+        locAim.transform.SetParent(ctrler.TargetContainer.transform);
 
         Target.SetActive(false);
 
@@ -211,7 +212,7 @@ public class LocalizationTask : BaseTask
 
     private void RotateLocalizer(float locX)
     {
-        ctrler.TargetContainer.transform.position =new Vector3 (targets[1].transform.position.x, ctrler.TargetContainer.transform.position.y, targets[1].transform.position.z);
+        //ctrler.TargetContainer.transform.position =new Vector3 (targets[1].transform.position.x, ctrler.TargetContainer.transform.position.y, targets[1].transform.position.z);
         if (ctrler.TargetContainer.transform.rotation.eulerAngles.y < 90){
             ctrler.TargetContainer.transform.Rotate(Vector3.up * locX * Time.deltaTime);
         }
@@ -232,7 +233,7 @@ public class LocalizationTask : BaseTask
     /// </summary>
     protected void Centre()
     {
-        ctrler.TargetContainer.transform.localPosition = Vector3.zero;
+        //ctrler.TargetContainer.transform.localPosition = Vector3.zero;
         Vector3 pos = targets[0].transform.position;
         Vector3 centre = pos - ctrler.transform.forward * 0.1f;
         ctrler.CentreExperiment(centre);
