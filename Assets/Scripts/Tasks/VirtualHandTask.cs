b39561b83aef005a70d840488abbe6d43c2ed1e3
Jorge de Oliveira <jorge.esteves.junior@gmail.com>
2022-11-15 16:45:38 -0500

    virtualhand prefab added and right hand avatar movement with wasd

--- Assets/Scripts/Tasks/VirtualHandTask.cs
+++ Assets/Scripts/Tasks/VirtualHandTask.cs
@@ -4,7 +4,35 @@ using UnityEngine;
 
 public class VirtualHandTask : BaseTask
 {
-    public override void Setup(){}
+    ExperimentController ctrler;
+    float speed = 0.75f;
+
+    GameObject virtualHandPrefab;
+    GameObject lHand;
+    GameObject rHand;
+    public override void Setup(){
+        maxSteps = 4;
+        ctrler = ExperimentController.Instance();
+
+        ctrler.CursorController.SetHandVisibility(false);
+        Cursor.visible = false;
+        ctrler.CursorController.SetCursorVisibility(true);
+
+        virtualHandPrefab = Instantiate(ctrler.GetPrefab("VirtualHandPrefab"));
+        lHand = GameObject.Find("ik_hand_l_target");
+        rHand = GameObject.Find("ik_hand_r_target");
+        //virtualHandPrefab.transform.localPosition = Vector3.zero;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-z, x, 0);
+        rHand.transform.Translate(movement * speed *Time.deltaTime);
+    }
 
     public override void Disable(){}
 
