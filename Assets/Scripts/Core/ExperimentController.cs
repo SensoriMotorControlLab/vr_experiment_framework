58224aea5147bd321166b0a6d8381b44f5a02150
Jorge de Oliveira <jorge.esteves.junior@gmail.com>
2023-03-07 17:10:36 -0500

    Adjusted height of centering function

--- Assets/Scripts/Core/ExperimentController.cs
+++ Assets/Scripts/Core/ExperimentController.cs
@@ -196,7 +196,7 @@ public class ExperimentController : MonoBehaviour
         exp_centre_pos = centre;
         Transform temp = cameraOffset.transform.parent;
         cameraOffset.transform.parent = dummyCamera.transform;
-        dummyCamera.transform.position = new Vector3(centre.x, 0.2f, centre.z);
+        dummyCamera.transform.position = new Vector3(centre.x, 0.3f, centre.z);
         dummyCamera.transform.eulerAngles = new Vector3(0, -10, 0);
         cameraOffset.transform.parent = null;
         cameraOffset.transform.parent = temp;
