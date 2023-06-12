8579bc03de37bc0211deaaa6cff1e62b4039a761
Jorge de Oliveira <jorge.esteves.junior@gmail.com>
2023-06-12 12:49:12 -0400

    center key change

--- Assets/Scripts/Core/ExperimentController.cs
+++ Assets/Scripts/Core/ExperimentController.cs
@@ -191,16 +191,16 @@ public class ExperimentController : MonoBehaviour
         }
     }
 
-    public void CentreExperiment(Vector3 centre)
-    {
-        exp_centre_pos = centre;
-        Transform temp = cameraOffset.transform.parent;
-        cameraOffset.transform.parent = dummyCamera.transform;
-        dummyCamera.transform.position = new Vector3(centre.x, 0.2f, centre.z);
-        dummyCamera.transform.eulerAngles = new Vector3(0, -10, 0);
-        cameraOffset.transform.parent = null;
-        cameraOffset.transform.parent = temp;
-        //StartCoroutine(TempDisableCursor());
+   public void CentreExperiment(Vector3 centre)
+    {
+        exp_centre_pos = centre;
+        Transform temp = cameraOffset.transform.parent;
+        cameraOffset.transform.parent = dummyCamera.transform;
+        dummyCamera.transform.position = new Vector3(centre.x, 0.2f, centre.z);
+        dummyCamera.transform.eulerAngles = new Vector3(0, -10, 0);
+        cameraOffset.transform.parent = null;
+        cameraOffset.transform.parent = temp;
+        //StartCoroutine(TempDisableCursor());
     }
 
     // Update is called once per frame
